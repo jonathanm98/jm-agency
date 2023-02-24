c4a77ac4febd51a8f104edd8acb417e5b8ce5864
jonathanm98 <jonathanmenelet@hotmail.fr>
2023-02-25 00:59:35 +0100

    Create pages components

---
+++ src/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="notFound">
+      <div className="notFound-content">
+        <h2>Erreur 404</h2>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
+<h1>404</h1>;
---
+++ src/pages/Project4.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Project4 = () => {
+  return (
+    <div>
+      <h1>Project4.js</h1>
+    </div>
+  );
+};
+
+export default Project4;
+<h1>Project4.js</h1>;
