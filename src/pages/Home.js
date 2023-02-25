4d384e3fa8737f6dadc3323d7bbf4ffcea088982
jonathanm98 <jonathanmenelet@hotmail.fr>
2023-02-25 02:11:15 +0100

    add animation title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,25 @@
 import React from "react";
+import DynamicText from "../components/DynamicText";
+import Mouse from "../components/Mouse";
 import Navigation from "../components/Navigation";
+import SocialNetwork from "../components/SocialNetwork";
 
 const Home = () => {
   return (
     <div>
-      <Navigation />
+      <Mouse />
+      <div className="home">
+        <Navigation />
+        <SocialNetwork />
+        <div className="home-main">
+          <div className="main-content">
+            <h1>JM Agency</h1>
+            <h2>
+              <DynamicText />
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
